1615e837ce116244e505435f4b39ab10c9ff1454
Academykw <cadetkw40@gmail.com>
2026-05-07 08:19:25 +0100

    setup adhan persistence

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,36 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audio_session/audio_session_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_remote_config_web/firebase_remote_config_web.dart';
+import 'package:flutter_timezone/flutter_timezone_web.dart';
+import 'package:geolocator_web/geolocator_web.dart';
+import 'package:just_audio_web/just_audio_web.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioSessionWeb.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseRemoteConfigWeb.registerWith(registrar);
+  FlutterTimezonePlugin.registerWith(registrar);
+  GeolocatorPlugin.registerWith(registrar);
+  JustAudioPlugin.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
