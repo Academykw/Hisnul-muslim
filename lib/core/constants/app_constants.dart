d6fc342381acaa245008ee473b2b66a48ef33c87
Academykw <cadetkw40@gmail.com>
2026-05-16 10:36:04 +0100

    restructure code/added multiple lang

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -15,7 +15,38 @@ class AppConstants {
   static const List<String> zakatCurrencies = ['USD', 'GBP', 'EUR', 'SAR', 'AED', 'MYR', 'NGN', 'AUD', 'CAD'];
   static const double zakatRate = 0.025;
 
-  // Firebase Fallbacks
+  // Firebase Fallbacks (Weekly Rotation)
+  static const List<Map<String, String>> fallbackInspirations = [
+    {
+      'content': "Indeed, with hardship [will be] ease.",
+      'source': "Quran 94:6",
+    },
+    {
+      'content': "When My servants ask you about Me, I am indeed near.",
+      'source': "Quran 2:186",
+    },
+    {
+      'content': "So remember Me; I will remember you.",
+      'source': "Quran 2:152",
+    },
+    {
+      'content': "And when you have decided, then rely upon Allah.",
+      'source': "Quran 3:159",
+    },
+    {
+      'content': "Unquestionably, by the remembrance of Allah hearts are assured.",
+      'source': "Quran 13:28",
+    },
+    {
+      'content': "And Allah would not punish them while they seek forgiveness.",
+      'source': "Quran 8:33",
+    },
+    {
+      'content': "Do not despair of the mercy of Allah. Indeed, Allah forgives all sins.",
+      'source': "Quran 39:53",
+    },
+  ];
+
   static const String fallbackInspirationContent = "Indeed, with hardship [will be] ease.";
   static const String fallbackInspirationSource = "Quran 94:6";
 
